db214318558fab66e8c986e57d5470758f16cbe9
Sam Shteinman <samshteinman@gmail.com>
2020-03-23 21:20:01 -0400

    initial commit

---
+++ ShareVibe/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  ShareVibe
+//
+//  Created by Sam Shteinman on 2020-03-13.
+//  Copyright © 2020 Sam Shteinman. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+import MediaPlayer
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        do
+        {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+        }
+        catch {}
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ ShareVibe/ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  ShareVibe
+//
+//  Created by Sam Shteinman on 2020-03-13.
+//  Copyright © 2020 Sam Shteinman. All rights reserved.
+//
+
+import SwiftUI
+import MediaPlayer
+import AVFoundation
+import FileProvider
+
+struct ContentView: View {
+    @State private var songs : MPMediaItemCollection?
+    @State private var showPicker : Bool = false
+    
+    @ObservedObject var CentralManager = BluetoothCentralManager()
+    @ObservedObject var PeripheralManager = PeripheralCentralManager()
+    
+    var body: some View {
+        VStack
+        {
+            Text("Connected: \(String(PeripheralManager.Connected || CentralManager.Connected))")
+            Button("Listening for Songs: \(self.CentralManager.Running.description)")
+            {
+                self.CentralManager.startup()
+            }
+            Button("Broadcast")
+            {
+                self.PeripheralManager.startup()
+                self.showPicker = !self.showPicker
+            }
+        }.sheet(isPresented: self.$showPicker,
+                onDismiss: self.sendSong)
+            {
+               SongPicker(songs: self.$songs)
+            }
+    }
+    
+    func sendSong()
+    {
+        if(self.songs == nil)
+        {
+            return
+        }
+        
+        let songItems: [MPMediaItem] = self.songs!.items
+        let songItem = songItems[0]
+        
+        if let url = songItem.value(forProperty: MPMediaItemPropertyAssetURL)
+        {
+            let asset = AVAsset(url: url as! URL)
+            NSLog("Compatible exports are : \(AVAssetExportSession.exportPresets(compatibleWith: asset))")
+          
+            let session = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetLowQuality)
+            session!.outputFileType = AVFileType.mp4
+            AVAssetExportSession.exportPresets(compatibleWith: asset)
+            
+            session?.shouldOptimizeForNetworkUse = true
+            session?.canPerformMultiplePassesOverSourceMediaData = true
+            
+            session!.outputURL = URL(fileURLWithPath: Globals.ExportedAudioFilePath.path)
+            
+            NSLog("Going to export to location: \(session!.outputURL!)")
+
+            clean(path: session!.outputURL!.path)
+
+            session?.exportAsynchronously(completionHandler:
+            {
+                if(session?.status == .completed)
+                {
+                    do
+                    {
+                        let data = try Data.init(contentsOf: session!.outputURL!)
+                        DispatchQueue.main.async
+                        {
+                            self.PeripheralManager.startSend(content: data)
+                        }
+                    }
+                    catch
+                    {
+                        NSLog("Could not fill buffer with data from \(session!.outputURL!) : \(error)")
+                    }
+                }
+                else
+                {
+                    NSLog("Failed for \(String(describing: session?.outputURL)) status: \(String(describing: session?.status.rawValue)) \(session?.error)")
+                }
+            })
+
+        }
+    }
+    
+    func clean(path : String)
+    {
+        do
+        {
+            if(FileManager.default.fileExists(atPath: path))
+            {
+                try FileManager.default.removeItem(atPath: path)
+                NSLog("Deleted old file at \(path)")
+            }
+            
+        }
+        catch
+        {
+            NSLog("Failed to remove file at \(path) : \(error)")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
